36ef08bdaee7ff9920f13d6d9848c0191f0ca33c
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-02-25 11:07:11 +0900

    チケットのリストを固定で表示

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Container } from '@mui/system';
 
 function App() {
   return (
-    <Button variant='contained' color='primary'>
-      Hello World!
-    </Button>
+    <Container maxWidth="lg">
+      <Typography sx={{mt: 4, mb: 2}} variant='h4' component='div'>TinyPDM</Typography>
+      <List dense={false}>
+        <ListItem key={1}>
+          <ListItemText primary='最初のチケット' />
+        </ListItem>
+        <ListItem key={2}>
+          <ListItemText primary='2番目のチケット' />
+        </ListItem>
+      </List>
+    </Container>
   );
 }
 
