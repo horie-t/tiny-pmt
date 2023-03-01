7f5fc378eb0e03808c8e8959257d9750b1e0e850
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-01 23:17:09 +0900

    springdoc-openapiでOpenAPI 3.0のyaml, jsonファイルを取得できるようにする
    何故かUI画面が正常に動作しない

--- backend/src/main/java/com/tehorie/tinypmt/presentation/TicketController.java
+++ backend/src/main/java/com/tehorie/tinypmt/presentation/TicketController.java
@@ -1,14 +1,21 @@
 package com.tehorie.tinypmt.presentation;
 
 import com.tehorie.tinypmt.presentation.model.TicketDTO;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@Tag(name = "Ticket", description = "The ticket API")
 public class TicketController {
 
     @GetMapping("/tickets")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     Flux<TicketDTO> ListTicket() {
         return Flux.fromArray(new TicketDTO[]{new TicketDTO("最初のチケット"), new TicketDTO("2番目のチケット")});
     }
