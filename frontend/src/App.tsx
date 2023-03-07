5714baad21f8570a472dda79d0223859b4163b71
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-08 08:03:14 +0900

    フロントエンド: Vite + Reactでプロジェクトを作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,34 @@
-import React from "react";
-import { Typography } from "@mui/material";
-import { Container } from "@mui/system";
-import TicketsComponent from "./components/TicketsComponent";
-import axios from "axios";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-  const [tickets, setTickets] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get("http://localhost:8080/tickets").then((response) => {
-      setTickets(response.data);
-    });
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <Container maxWidth="lg">
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h4" component="div">
-        TinyPMT
-      </Typography>
-      {(() =>  {
-        if (tickets != null) {
-          return <TicketsComponent tickets={tickets} />;}
-        else { 
-          return "";
-        }})()}
-    </Container>
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
-export default App;
+export default App
