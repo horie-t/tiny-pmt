9c56352235f1f469bfc0694a35ad9f48092fa9cf
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-11 17:41:35 +0900

    フロントエンド: チケット作成ボタンを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import ButtonAppBar from './components/ButtonAppBar';
 import TicketsComponent from './components/TicketsComponent';
 import axios from "axios";
+import { Button, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { AddCircle } from '@mui/icons-material';
 
 export default function App() {
   const [tickets, setTickets] = React.useState(null);
@@ -15,7 +18,12 @@ export default function App() {
   return (
     <div>
       <ButtonAppBar></ButtonAppBar>
-      {tickets != null ? <TicketsComponent tickets={tickets} /> : ""}
+      <Typography variant='h5' sx={{m: 1}}>チケット作成</Typography>
+      <Box sx={{ml: 2}}>
+        <Button variant='contained' startIcon={<AddCircle></AddCircle>}>チケットを作成</Button>
+      </Box>
+      <Typography variant='h5' sx={{mt: 1, ml: 1}}>チケット一覧</Typography>
+      {tickets != null ? <TicketsComponent tickets={tickets} /> : "チケットがありません。"}
     </div>
   );
 }
