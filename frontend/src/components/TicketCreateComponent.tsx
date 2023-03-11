c02bb00333f13a755ca58c951e9a688aba88124c
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-11 19:24:12 +0900

    React Hook Formで入力内容をコントロールする
    作成処理のタイミングでコンソールログを出すだけ

--- frontend/src/components/TicketCreateComponent.tsx
+++ frontend/src/components/TicketCreateComponent.tsx
@@ -7,15 +7,27 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { AddCircle } from '@mui/icons-material';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Stack } from '@mui/material';
+
+type Ticket = {
+  title: string,
+  description: string,
+}
 
 export default function TicketCreate() {
-  const [open, setOpen] = React.useState(false);
+  const { control, handleSubmit, reset } = useForm<Ticket>();
+  const onSubmit: SubmitHandler<Ticket> = (ticket) => {
+    console.log(ticket);
+    handleClose();
+  };
 
+  const [open, setOpen] = React.useState(false);
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
+    reset();
     setOpen(false);
   };
 
@@ -25,18 +37,24 @@ export default function TicketCreate() {
         チケットを作成
       </Button>
       <Dialog fullWidth maxWidth="md" open={open} onClose={handleClose}>
-        <DialogTitle>チケット作成</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            チケットを作成します。
-          </DialogContentText>
-          <TextField required autoFocus margin="dense" id="title" type="text" fullWidth label="タイトル" variant="outlined" />
-          <TextField  autoFocus margin="dense" id="title" type="text" fullWidth label="説明" variant="outlined" multiline rows={5}/>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>キャンセル</Button>
-          <Button onClick={handleClose}>作成</Button>
-        </DialogActions>
+        <Stack component="form" onSubmit={handleSubmit(onSubmit)}>
+          <DialogTitle>チケット作成</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              チケットを作成します。
+            </DialogContentText>
+            <Controller name="title" control={control} render={({ field }) => 
+             <TextField required autoFocus margin="dense" id="title" type="text" fullWidth 
+              label="タイトル" variant="outlined" {...field} />} />
+            <Controller name="description" control={control} render={({field}) => 
+            <TextField  autoFocus margin="dense" id="title" type="text" fullWidth 
+              label="説明" variant="outlined" multiline rows={5} {...field}/>} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>キャンセル</Button>
+            <Button type='submit'>作成</Button>
+          </DialogActions>
+        </Stack>
       </Dialog>
     </div>
   );
