d8f1c1bdde317c0bb23c9345d31bc8af6ee50a4f
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-11 12:53:20 +0900

    バックエンド: OpenAPIの仕様書にdescriptionを追加

--- backend/src/main/java/com/tehorie/tinypmt/presentation/dto/TicketResponse.java
+++ backend/src/main/java/com/tehorie/tinypmt/presentation/dto/TicketResponse.java
@@ -1,9 +1,14 @@
 package com.tehorie.tinypmt.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(description = "チケットです。")
 public class TicketResponse extends TicketRequestBody {
+    @Size(min = 13, max = 13)
+    @Schema(description = "IDです。")
     private String id;
 
     public TicketResponse(String id, String title, String description) {
