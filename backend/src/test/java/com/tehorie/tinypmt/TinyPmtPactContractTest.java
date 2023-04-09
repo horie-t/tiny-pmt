c75507a318f87983819a65068de5d875df6fbb62
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-04-09 09:47:53 +0900

    バックエンド: PactのMockはリポジトリクラスにする

--- backend/src/test/java/com/tehorie/tinypmt/TinyPmtPactContractTest.java
+++ backend/src/test/java/com/tehorie/tinypmt/TinyPmtPactContractTest.java
@@ -6,11 +6,18 @@ import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import com.tehorie.tinypmt.application.TicketService;
+import com.tehorie.tinypmt.domain.model.Ticket;
+import com.tehorie.tinypmt.domain.model.TicketRepository;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=8080")
@@ -21,6 +28,9 @@ public class TinyPmtPactContractTest {
     @Autowired
     private TicketService ticketService;
 
+    @MockBean
+    private TicketRepository ticketRepository;
+
     @TestTemplate
     @ExtendWith(PactVerificationSpringProvider.class)
     public void pactVerificationTestTemplate(PactVerificationContext context) {
@@ -29,5 +39,8 @@ public class TinyPmtPactContractTest {
 
     @State(value = {"チケットリストを作成する"})
     public void TicketList() {
+        Mockito.when(ticketRepository.findAll()).thenReturn(Flux.just(
+                new Ticket(UUID.randomUUID(), "title1", "desc1", false),
+                new Ticket(UUID.randomUUID(), "title2", "desc2", false)));
     }
 }
